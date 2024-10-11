4b4e3fc979d2e07153ed717b5bd24b08d9c8f8c1
gokul <gokulchandan24@gmail.com>
2024-10-12 01:17:23 +0530

    steps added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Recursive } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const recursive = Recursive({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -21,8 +22,10 @@ export default function RootLayout({
         <Navbar />
         <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">{children}</div>
+          <Footer />
         </main>
-        <Footer />
+
+        <Toaster />
       </body>
     </html>
   );
